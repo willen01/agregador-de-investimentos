7885e9a7730afff5486411eb21936e71bc4627c7
willen <willen.sesantos@gmail.com>
2024-01-06 14:16:09 -0300

    chore: Add user repository

---
+++ src/main/java/com/willen/agregadorinvestimentos/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.willen.agregadorinvestimentos.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.willen.agregadorinvestimentos.entities.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+}
