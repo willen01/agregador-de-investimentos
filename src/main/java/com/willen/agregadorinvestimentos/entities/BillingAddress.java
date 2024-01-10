57592565d50e123570cd6a9ae5415cc49d460a49
willen <willen.sesantos@gmail.com>
2024-01-10 10:26:50 -0300

    chore: Add BillingAddress entity

---
+++ src/main/java/com/willen/agregadorinvestimentos/entities/BillingAddress.java
@@ -0,0 +1,56 @@
+package com.willen.agregadorinvestimentos.entities;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_billingAddress")
+public class BillingAddress {
+
+    @Id
+    private UUID id;
+
+    @Column(name = "street")
+    private String street;
+
+    @Column(name = "number")
+    private Integer number;
+
+    public BillingAddress() {
+    }
+
+    public BillingAddress(UUID id, String street, Integer number) {
+        this.id = id;
+        this.street = street;
+        this.number = number;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+}
