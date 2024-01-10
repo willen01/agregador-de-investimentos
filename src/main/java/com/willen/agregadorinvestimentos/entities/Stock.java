71635f3e836b805975ef75497c55556cce04cc09
willen <willen.sesantos@gmail.com>
2024-01-10 11:17:25 -0300

    chore: Add Stock entitie

---
+++ src/main/java/com/willen/agregadorinvestimentos/entities/Stock.java
@@ -0,0 +1,43 @@
+package com.willen.agregadorinvestimentos.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_stock")
+public class Stock {
+
+    @Id
+    @Column(name = "stock_id")
+    private String stockId;
+
+    @Column(name = "description")
+    private String description;
+
+    public Stock() {
+    }
+
+    public Stock(String stockId, String description) {
+        this.stockId = stockId;
+        this.description = description;
+    }
+
+    public String getStockId() {
+        return stockId;
+    }
+
+    public void setStockId(String stockId) {
+        this.stockId = stockId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
