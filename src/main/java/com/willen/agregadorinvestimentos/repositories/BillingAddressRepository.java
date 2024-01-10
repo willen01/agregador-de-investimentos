652cd2c4b097661288543282eb41a4c960a73f9f
willen <willen.sesantos@gmail.com>
2024-01-10 17:05:40 -0300

    fix: Fix type for repository

--- src/main/java/com/willen/agregadorinvestimentos/repositories/BillingAddressRepository.java
+++ src/main/java/com/willen/agregadorinvestimentos/repositories/BillingAddressRepository.java
@@ -5,7 +5,9 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.willen.agregadorinvestimentos.entities.BillingAddress;
+
 @Repository
-public interface BillingAddressRepository extends JpaRepository<BillingAddressRepository, UUID> {
+public interface BillingAddressRepository extends JpaRepository<BillingAddress, UUID> {
 
 }
