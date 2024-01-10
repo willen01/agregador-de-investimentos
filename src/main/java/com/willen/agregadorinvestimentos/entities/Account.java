3511465119f9056634088f469905daea4f59ce2b
willen <willen.sesantos@gmail.com>
2024-01-09 22:55:25 -0300

    chore: Add Account entitie

---
+++ src/main/java/com/willen/agregadorinvestimentos/entities/Account.java
@@ -0,0 +1,47 @@
+package com.willen.agregadorinvestimentos.entities;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_accounts")
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "account_id")
+    private UUID accountId;
+
+    @Column(name = "description")
+    private String descriprion;
+
+    public Account() {
+    }
+
+    public Account(UUID accountId, String descriprion) {
+        this.accountId = accountId;
+        this.descriprion = descriprion;
+    }
+
+    public UUID getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(UUID accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getDescriprion() {
+        return descriprion;
+    }
+
+    public void setDescriprion(String descriprion) {
+        this.descriprion = descriprion;
+    }
+}
