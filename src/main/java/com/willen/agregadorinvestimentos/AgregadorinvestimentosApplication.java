95454157c372663dfa0525dc86bb6cdfdcaac840
willen <willen.sesantos@gmail.com>
2024-01-11 13:39:47 -0300

    chore: Add OpenFeign

--- src/main/java/com/willen/agregadorinvestimentos/AgregadorinvestimentosApplication.java
+++ src/main/java/com/willen/agregadorinvestimentos/AgregadorinvestimentosApplication.java
@@ -2,8 +2,10 @@ package com.willen.agregadorinvestimentos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class AgregadorinvestimentosApplication {
 
 	public static void main(String[] args) {
